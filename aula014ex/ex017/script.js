e6e9e4c726f3f0a6fb79a871e01d602c8ada627b
Fernando <barbaglianeto@gmail.com>
2025-04-17 15:23:14 -0300

    mais uma aula hoje foi lgl

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,19 @@
-window.alert("virando a noite estudando e jogando valorant/r6")
+function calculate(){
+    const num = document.getElementById('txtnum')
+    const resp = document.getElementById('seltab')
+    if(num.value.length == 0){ // verifica se o cliente digitou algum numero
+        alert('type a number to start')
+    } else{
+        const num = Number(txtnum.value) 
+        resp.innerHTML = ''
+        let c = 1
+        while(c <= 10){
+            let item = document.createElement('option') // cria um elemento na parte HTML do site porem nao mostra
+            item.text = `${num} x ${c} = ${num * c}`
+            item.value = `tab${c}`
+            resp.appendChild(item) // mostra na tela o elemento criado anteriormente
+            c++ // contagem
+        }
+    }
+
+}
